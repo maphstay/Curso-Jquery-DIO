ba3c9875abc3597b7fce94f597be58064164ab37
Stefferson Thallys <steffersonthallys@gmail.com>
2021-03-19 21:48:04 -0300

    Update main.js
    condição para valor vazio no input

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
 function consultaCep() {
-    $(".barra-progresso").show();
-    let cep = document.getElementById("cep").value
-    $.ajax({
-        url: `https://viacep.com.br/ws/${cep}/json/`,
-        type: "GET",
-        success: function(response){
-            console.log(response)
-            $("#logradouro").html(response.logradouro);
-            $("#bairro").html(response.bairro);
-            $("#localidade").html(response.localidade);
-            $("#uf").html(response.uf);
-            $("#titulo").html("CEP " + response.cep);
-            $(".teste").show();
-            $(".barra-progresso").hide();
-        }
-    })  
     
+    let cep = document.getElementById("cep").value
+    if (cep === "") {
+        alert("Digite um valor!!!")
+    }else {
+        $(".barra-progresso").show();
+        $.ajax({
+            url: `https://viacep.com.br/ws/${cep}/json/`,
+            type: "GET",
+            success: function(response){
+                console.log(response)
+                $("#logradouro").html(response.logradouro);
+                $("#bairro").html(response.bairro);
+                $("#localidade").html(response.localidade);
+                $("#uf").html(response.uf);
+                $("#titulo").html("CEP " + response.cep);
+                $(".teste").show();
+                $(".barra-progresso").hide();
+            }
+        })  
+    }
 }
 
 $(function(){
